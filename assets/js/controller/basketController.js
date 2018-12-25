b17eda8ce297179f39c97a8c63156bf00284b05e
Unknown <alain.holzhauser@mmp.htwchur.ch>
2018-12-26 00:51:53 +0100

    - add salad dressing

--- assets/js/controller/basketController.js
+++ assets/js/controller/basketController.js
@@ -3,14 +3,24 @@ function storeArticle(e) {
         let articleId = e.target.getAttribute('data-id');
         let article = e.target.getAttribute('data-name');
         let articlePrize = e.target.getAttribute('data-prize');
+        let articleType = e.target.getAttribute('data-type');
 
         let articleAmount = e.target.parentElement.previousSibling.previousSibling.value;
+
+        if(articleType ==="salads") {
+        articleType =  e.target.parentElement.previousSibling.previousSibling.previousSibling.previousSibling.value;
+        }
+       
+
+        console.log(articleType);
+
         let orders =   localStorage.getItem('orders');
 
         let newOrder = {
             'id':      articleId,
             'article': article,
             'amount':  articleAmount,
+            'type'  : articleType,
             'prize': articlePrize
         }
 
